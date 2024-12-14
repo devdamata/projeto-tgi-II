2c8928af0467285f4b6e2013dcaa28b93c10054f
Anderson da Mata <anderson.daamata@gmail.com>
2024-12-13 21:25:34 -0300

    ajustes cors v3

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 
 return [
     'supports_credentials' => true,
-    'allowed_origins' => ['https://tgi-projeto-front-end-git-main-devdamatas-projects-6bfc92be.vercel.app'], // URL do seu front-end
+    'allowed_origins' => ['https://tgi-projeto-front-end-git-main-devdamatas-projects-6bfc92be.vercel.app', 'https://tgi-projeto-front-end.vercel.app', 'http://localhost:3000'], // URL do seu front-end
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
